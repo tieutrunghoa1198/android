6a5fa085e38774b5943ffca3fe1d77c3b1b7e74b
hoatt68@viettel.com.vn <hoatt68@ViettelGroup.ViettelAD.com>
2022-03-14 17:30:12 +0700

    a

---
+++ app/src/main/java/com/example/shoeapp/login/LogInService.java
@@ -0,0 +1,5 @@
+package com.example.shoeapp.login;
+
+public class LogInService {
+
+}
--- app/src/main/java/com/example/shoeapp/login/Register.java
+++ app/src/main/java/com/example/shoeapp/login/Register.java
@@ -1,13 +1,20 @@
 package com.example.shoeapp.login;
 
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.shoeapp.R;
 
-public class Register extends AppCompatActivity {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class Register extends AppCompatActivity {
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +22,24 @@ public class Register extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(R.id.spinnerDays);
         Spinner spinnerMonths = (Spinner) findViewById(R.id.spinnerMonths);
         Spinner spinnerYears = (Spinner) findViewById(R.id.spinnerYears);
+
+        this.btnRegister = findViewById(R.id.btnRegister);this.btnRegisterHandler(btnRegister);
+        this.btnRegisterHandler(btnRegister);
         this.initDropDown(spinner, spinnerMonths, spinnerYears);
+    }
+
+    private void btnRegisterHandler(Button btn){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    URL url = new URL("https://linken.tranduydat.com/prm/api/v1.0/product/images/?product_id=1");
+                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void initDropDown(Spinner days, Spinner months, Spinner years) {
