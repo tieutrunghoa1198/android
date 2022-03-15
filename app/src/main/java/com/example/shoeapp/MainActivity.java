eb0ec12430a919ea76a9eb846e7ba3da172981c2
hoatt68@viettel.com.vn <hoatt68@ViettelGroup.ViettelAD.com>
2022-03-15 17:38:58 +0700

    update login fn

--- app/src/main/java/com/example/shoeapp/MainActivity.java
+++ app/src/main/java/com/example/shoeapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,8 +23,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvRegister;
-    Button btnSignIn;
+    private TextView tvRegister;
+    private Button btnSignIn;
+    private EditText etUsername;
+    private EditText etPassword;
     private List<User> listUsers = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,41 +34,78 @@ public class MainActivity extends AppCompatActivity {
         this.listUsers.add(new User("tieuhoa2", "tieuhoa"));
         this.listUsers.add(new User("tieuhoa3", "tieuhoa"));
         this.listUsers.add(new User("tieuhoa4", "tieuhoa"));
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
         this.btnSignIn = findViewById(R.id.btnSignIn);
-        tvRegister = findViewById(R.id.tvRegister);
+        this.tvRegister = findViewById(R.id.tvRegister);
+        this.etUsername = findViewById(R.id.etEmail);
+        this.etPassword = findViewById(R.id.etPassword);
+        this.etUsername = findViewById(R.id.etEmail);
+        this.etPassword = findViewById(R.id.etPassword);
 
-        /*
-         * =============== START OF INTERACTING FUNCTIONS ===============
-         *
-         * */
+        this.btnLoginHandler(btnSignIn, etUsername, etPassword);
+        this.tvRegisterHandler();
+    }
 
-        this.btnSignIn.setOnClickListener(new View.OnClickListener() {
+
+    /*
+     * =============== START OF INTERACTING FUNCTIONS ===============
+     *
+     * */
+    private void btnLoginHandler(Button btn, EditText etUsername, EditText etPassword){
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Register.class);
-                startActivity(intent);
-                System.out.println("vao day nhe");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                boolean isUsernameEmpty = etUsername.getText().toString().isEmpty();
+                boolean isPassEmpty = etPassword.getText().toString().isEmpty();
+                if (isUsernameEmpty && isPassEmpty) {
+                    System.out.println("empty");
+                    Toast.makeText(getApplicationContext(), "Please enter username and password, then try again!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                login(username, password, listUsers);
+                if (login(username, password, listUsers)) {
+                    Intent intent = new Intent(MainActivity.this, Register.class);
+                    startActivity(intent);
+                    System.out.println("vao day nhe");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong email & password. Please try till you die!", Toast.LENGTH_LONG).show();
+                    System.out.println("wrong pass");
+                }
             }
         });
+    }
 
+    private void tvRegisterHandler() {
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "textt view component has been clicked", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, Register.class);
                 startActivity(intent);
-                System.out.println("vao day nhe");
             }
         });
+    }
 
-        /*
-         * =============== END OF INTERACTING FUNCTIONS ===============
-         *
-         * */
+
+    /*
+    * =============== START OF LOGIC FUNCTIONS ===============
+    * */
+    private boolean login(String username, String password, List<User> listData) {
+        if (listData.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Data Empty", Toast.LENGTH_LONG).show();
+        }
+        for(User u : listData) {
+            if (username.equals(u.getUsername()) && password.equals(u.getPassword())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
